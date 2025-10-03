ad0ab701b455d1541cf7eda7a08fa3888b4cce14
st <st@st.com>
2025-10-03 10:31:18 -0300

    Debug

--- js/gallery.js
+++ js/gallery.js
@@ -16,7 +16,7 @@
     button.appendChild(counter);
     let itemCount = 0;
     function updateCounter() {
-      counter.textContent = `Items Loaded: ${itemCount}`;
+      counter.innerHTML = `Items Loaded: ${itemCount}<br>Loading Status: ${isLoading}/${endOfList}<br>Scroll Height:${article.scrollHeight}/${article.clientHeight}+200`;
     }
     function shuffle(arr) {
       for (let i = arr.length - 1; i > 0; i--) {
